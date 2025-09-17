9efe8b7adc390decc9aa9469cf0772dc42924559
usman <usman@example.com>
2025-09-17 20:37:36 +0500

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:myappac/registerview.dart';
+import 'package:myappac/views/registerview.dart';
 
 
 void main() {
